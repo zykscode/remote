d40479dc15b3a4736c22bf88b1b668005d334e6b
zykscode <111462249+zykscode@users.noreply.github.com>
2022-11-23 23:42:01 +0100

    feat: Initial commit

---
+++ Ui/Components/ThemeToggle.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+type Props = {}
+
+const ThemeToggle = (props: Props) => {
+   const [mounted, setMounted] = useState(false)
+    
+  return (
+    <div>ThemeToggle</div>
+  )
+}
+
+export default ThemeToggle
---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+const Page = (props: Props) => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Page
